5f91fdf5058a47eaa4c1eae3b6934791b1768b57
Megaoski <oscar_2596@hotmail.com>
2021-10-04 20:29:09 +0200

    Playing with Threads(WIP)

--- Networks&OnlineGames1/Assets/Threads.cs
+++ Networks&OnlineGames1/Assets/Threads.cs
@@ -6,35 +6,62 @@ using System.Threading;
 
 public class Threads : MonoBehaviour
 {
+
+    GameObject CubeObject;
+
+    Vector3 pos;
+    float movement_speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Thread myThread = new Thread(TestThread);
-        myThread.Start();
+        CubeObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        lock(CubeObject)
+        {
+            CubeObject.transform.position = pos;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Key Down");
+
+            Thread myThread = new Thread(MoveObject);
+            myThread.Start();     
+            
+            Debug.Log("Finished Key Down");
+        }
+
     }
+    
 
-    void TestThread()
+    public void MoveObject()
     {
-        Debug.LogWarning("STARTING THREAD");
-
+        Debug.LogWarning("Starting Moving Thread");
         System.DateTime myTime;
 
-        while (!exit)
-        {
-            myTime = System.DateTime.UtcNow;
-            
-            while((System.DateTime.UtcNow-myTime).Seconds < 5f)
-            {
+        Vector3 speed = new Vector3(0f, 0f, 2f);
 
+        myTime = System.DateTime.UtcNow;
+        while((System.DateTime.UtcNow-myTime).Seconds < 5f)
+        {
+            //translate object
+            lock (CubeObject)
+            {
+                pos += Vector3.forward * movement_speed;
             }
-            Debug.Log("5s passed");
-                
+            
         }
+
+        Debug.Log("5s passed");
+
+        Debug.Log("Finished Thread");
     }
+
+    
 }
