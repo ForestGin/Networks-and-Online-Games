68001ee417c76f58bc340222292585d4ecd592d9
Pol Bosch <pol.hcsob@gmail.com>
2021-10-15 23:21:39 +0200

    Created Server and client from yt tutorial

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            Console.WriteLine("Hello World!");
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
